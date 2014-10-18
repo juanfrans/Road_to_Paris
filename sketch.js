4d820afcf8945953fb4d904df5002d10c6267ae6
Juan Francisco Saldarriaga <juanfrans@gmail.com>
2014-10-17 23:10:35 -0400

    Minor fixes to position and global variables

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 //Global variables
+var margin = 20;
 var canvasWidth = 1280;
 var canvasHeight = 720;
 //var mapStartX = -17018188;
@@ -12,6 +13,7 @@ var positionY;
 var ellipseSize = 2;
 var startX = 20;
 var date = 50;
+var mapWidth, mapHeight;
 
 //Preload images and data
 function preload(){
@@ -25,6 +27,8 @@ function setup() {
 	createCanvas(canvasWidth, canvasHeight);
 	colorMode(HSB, 360, 100, 100, 100);
 	background(100);
+	mapWidth = canvasWidth-margin*2;
+	mapHeight = mapWidth/img.width*img.height;
 	//var projectedCoord = [];
 	//var newProjectedCoord = [];
 	//console.log("done converting stuff");
@@ -33,27 +37,34 @@ function setup() {
 function draw(){
 	background(100);
 
+	//Temp Border
+	noFill();
+	stroke(0);
+	strokeWeight(1);
+	rect(margin, margin, mapWidth, mapHeight);
+
 	//Load map image
-	image(img, 0, 0, 1280, (1280/img.width)*img.height);
+	image(img, margin, margin, mapWidth, mapHeight);
 
 	//Loop through the data points and plot the events
 	for (var i = events.getRowCount() - 1; i >= 1; i--) {
 		if(date == events.getColumn(5)[i] || date == events.getColumn(6)[i] || date == events.getColumn(7)[i] || date == events.getColumn(8)[i]){
-			positionX = events.getColumn(3)[i];
-			positionY = events.getColumn(4)[i];
+			positionX = map(events.getColumn(3)[i], 0, 1280, 0, mapWidth);
+			//console.log(i+" "+margin+" "+img.width+" "+mapWidth+" "+positionX+" "+events.getColumn(3)[i]);
+			positionY = map(events.getColumn(4)[i], 0, 657, 0, mapHeight);
 			fill(35, 100, 100);
 			noStroke();
-			ellipse(positionX, positionY, 8, 8);
+			ellipse(margin+positionX, margin+positionY, 8, 8);
 			stroke(35,100,100);
 			strokeWeight(.75);
-			line(positionX*1, positionY*1-5, positionX*1, positionY*1-35);
-			line(positionX*1, positionY*1-35, positionX*1+125, positionY*1-35);
+			line(margin+positionX*1, margin+positionY*1-5, margin+positionX*1, margin+positionY*1-35);
+			line(margin+positionX*1, margin+positionY*1-35, margin+positionX*1+125, margin+positionY*1-35);
 			noStroke();
 			fill(30);
 			textSize(10);
-			text(events.getColumn(0)[i], positionX*1+7, positionY*1-38);
+			text(events.getColumn(0)[i], margin+positionX*1+7, margin+positionY*1-38);
 			textSize(8);
-			text(events.getColumn(1)[i], positionX*1+7, positionY*1-38, 120, 100);
+			text(events.getColumn(1)[i], margin+positionX*1+7, margin+positionY*1-38, 120, 100);
 		}else{}
 	}
 	//Toggle Icons
